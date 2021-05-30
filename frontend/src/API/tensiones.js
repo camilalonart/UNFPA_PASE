a3bcfa36f07ada4f4ac3582bb7e1ecefbdfca075
Camila Londoño <camilalonart@Camilas-MacBook-Air.local>
2021-05-30 19:16:59 -0400

    get solved

--- frontend/src/API/tensiones.js
+++ frontend/src/API/tensiones.js
@@ -2,7 +2,44 @@ import axios from 'axios';
 
 const url = '/tensiones';
 
-export const fetchTensiones = () => axios.get(url);
-export const createTension = (newTension) => axios.post(url, newTension);
-export const updateTension = (id, updatedTension) => axios.patch(`${url}/${id}`, updatedTension);
-export const deleteTension = (id) => axios.delete(`${url}/${id}`);
+export const getTensiones = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get(url);
+
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createTension = (tension) => async (dispatch) => {
+  try {
+    const { data } = await axios.post(url, newTension);
+
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const updateTension = (id, tension) => async (dispatch) => {
+  try {
+    const { data } = await axios.patch(`${url}/${id}`, updatedTension);
+
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
+export const deleteTension = (id) => async (dispatch) => {
+  try {
+    await await axios.delete(`${url}/${id}`);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
